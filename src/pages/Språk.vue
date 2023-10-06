6c4f9447a38851729e396ca2f779479c9404501c
haabra-asvg <e-haabra2@iskule.no>
2023-10-06 10:50:10 +0200

    Endret språk siden til å være lik utdanning siden

--- src/pages/Språk.vue
+++ src/pages/Språk.vue
@@ -1,44 +1,14 @@
 <script setup>
 
+  import Props from '../components/SpråkComponent.vue';
+  import JSON from '../assets/json/språk.json';
+
 </script>
 
 <template>
-
-  <div class="container">
-    <div class="vue">
-      <h2>Vue</h2>
-      <p>Vue er et JavaScript-rammeverk som gjør det enklere å programmere i JavaScript. Blant annet er denne nettsiden laget i VueJS</p>
-    </div>
-    <div class="js">
-      <h2>JavaScript</h2>
-      <p>I JavaScript har jeg kunnskap til nettsidelaging og litt NodeJS. Valgomaten min er laget i JavaScript.</p>
-    </div>
-    <div class="php">
-      <h2>PHP</h2>
-      <p>Jeg brukte PHP i VG1, der har jeg stort sett arbeidet med databaser (phpMyAdmin).</p>
-    </div>
-    <div class="html&css">
-      <h2>HTML & CSS</h2>
-      <p>Alle nettsider du lager krever du HTML & CSS. Jeg har god kjennskap til HTML og grei kjennskap til CSS. Jeg prøver å gjøre med bedre i CSS ofte.</p>
-    </div>
-  </div>
-
+  <Props v-for="i in JSON" :title="i.title" :description="i.description" :id="i.id" />
 </template>
 
 <style scoped>
-
-  .container {
-    padding-top: 2.5%;
-    display: block;
-    width: 50%;
-    margin: 0 auto;
-    text-align: center;
-  }
-
-  .container div {
-    margin-top: 1.25%;
-    margin-bottom: 1.25%;
-    border: 1px solid black;
-  }
 
 </style>
